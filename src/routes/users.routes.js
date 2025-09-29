5d7865941393f8c0ac796d61c0eb0c83c5b1e8da
Amin_117 <garridoamin@gmail.com>
2025-09-29 10:58:31 -0300

    rutas de user completas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import { deleteUser, getAllUsers } from "../controllers/users.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { validator } from "../middlewares/validator.middleware.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
 
 export const userRoutes = Router();
 
 // TODO: proteger las rutas con middlewares de autenticación y autorización faltantes
 
 // * traer todos los usuarios (usuario autenticado que sea admin)
-userRoutes.get("/users", getAllUsers);
+userRoutes.get(
+  "/users",
+  authMiddleware,
+  adminMiddleware,
+  validator,
+  getAllUsers
+);
 
 // * eliminar un usuario por id (usuario autenticado que sea admin)
-userRoutes.delete("/users/:id", deleteUser);
+userRoutes.delete(
+  "/users/:id",
+  authMiddleware,
+  adminMiddleware,
+  validator,
+  deleteUser
+);

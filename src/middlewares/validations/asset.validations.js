63b337c9b417da767e04fc2c496f997d66e24c41
Amin_117 <garridoamin@gmail.com>
2025-09-29 11:23:50 -0300

    Assets middleware listo

--- src/middlewares/validations/asset.validations.js
+++ src/middlewares/validations/asset.validations.js
@@ -1,3 +1,40 @@
 export const createAssetValidation = [
   // TODO: completar las validaciones para crear un recurso
+  body("inventoryNumber")
+    .isString()
+    .withMessage("El nombre de usuario debe ser una cadena de texto")
+    .notEmpty()
+    .withMessage("El nombre de es obligatorio"),
+  body("description")
+    .isString()
+    .withMessage("La descripcion debe ser una cadena de texto")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripcion no puede exceder los 500 caracteres"),
+  body("brand")
+    .isString()
+    .withMessage("La brand debe ser una cadena de texto")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("La brand no puede exceder los 100 caracteres"),
+  body("model")
+    .isString()
+    .withMessage("El model debe ser una cadena de texto")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El model no puede exceder los 100 caracteres"),
+  body("status")
+    .optional()
+    .isIn("good", "regular", "bad", "out_of_service")
+    .withMessage("El status debe ser valido"),
+  body("acquisitionDate")
+    .isISO8601()
+    .notEmpty()
+    .withMessage("El campo es obligatorio")
+    .withMessage("La fecha de debe ser válida"),
+  body("acquisitionValue")
+    .isIn()
+    .withMessage("Tiene que ser un numero")
+    .notEmpty()
+    .withMessage("El campo es obligatorio")
+    .isLength({ min: 0 })
+    .trim()
+    .escape(),
 ];
--- src/routes/assets.routes.js
+++ src/routes/assets.routes.js
@@ -7,8 +7,13 @@ import {
 } from "../controllers/assets.controller.js";
 import { createAssetValidation } from "../middlewares/validations/asset.validations.js";
 import { validator } from "../middlewares/validator.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
+import { responsibleMiddleware } from "../middlewares/responsible.middleware.js";
 
 export const assetRoutes = Router();
+
+assetRoutes.use(authMiddleware);
 
 // TODO: proteger las rutas con middlewares de autenticación y autorización faltantes
 
@@ -16,10 +21,20 @@ export const assetRoutes = Router();
 assetRoutes.post("/assets", createAssetValidation, validator, createAsset);
 
 // * traer todos los recursos (usuario autenticado que sea admin)
-assetRoutes.get("/assets", getAllAssets);
+assetRoutes.get("/assets", adminMiddleware, validator, getAllAssets);
 
 // * traer mis recursos (usuario autenticado que sea responsible)
-assetRoutes.get("/assets/my-assets", getMyAssets);
+assetRoutes.get(
+  "/assets/my-assets",
+  responsibleMiddleware,
+  validator,
+  getMyAssets
+);
 
 // * eliminar un recurso por id (usuario autenticado que sea responsible)
-assetRoutes.delete("/assets/:id", deleteAsset);
+assetRoutes.delete(
+  "/assets/:id",
+  responsibleMiddleware,
+  validator,
+  deleteAsset
+);
